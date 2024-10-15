9d40b0bee3630977e94b28ffdbb60b267802872d
Daniel Vieira <danielccobr@gmail.com>
2024-10-15 20:23:39 -0300

    dessa vez sem force

--- src/Participante.java
+++ src/Participante.java
@@ -2,39 +2,31 @@ public class Participante {
     private String nome;
     private String email;
     private String tipo; // normal ou VIP
-
     // Construtor
     public Participante(String nome, String email, String tipo) {
         this.nome = nome;
         this.email = email;
         this.tipo = tipo;
     }
-
     // Getters e Setters
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getTipo() {
         return tipo;
     }
-
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
-
     @Override
     public String toString() {
         return "Nome: " + nome + ", Email: " + email + ", Tipo: " + tipo;
